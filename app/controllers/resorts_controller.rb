0c9db0047416fea8139427b688d4b7530fb08875
aurelien rothan <rothan.aurelien@gmail.com>
2020-08-26 12:10:22 +0200

    modif_controller_resort

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -1,4 +1,6 @@
 class ResortsController < ApplicationController
+    skip_before_action :authenticate_user!, only: [ :index, :show ]
+
     def index
         @resorts = Resort.all
     end
@@ -7,5 +9,14 @@ class ResortsController < ApplicationController
         @resort = Resort.new
         @resort = resort.find(params[:id])
         @activity = Activity.new
+    end
+
+    def set_resort
+        @resort = Resort.find(params[:id])
+    end
+
+    def resort_params
+        params.require(:resort).permit(:resort_name, :resort_addres, :resort_description, :visitor_name, :roommate, :budget, 
+        :day_activities, :biological_clock, :accommodation, :spot, :night_activities, :expectations, :geocation, :kilometers)
     end
 end
