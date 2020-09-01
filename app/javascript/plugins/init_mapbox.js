9126e93bc520dab74f876d8b51ef0c3498b50f5f
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2020-09-01 13:19:19 +0200

    titre station et massif

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -22,7 +22,7 @@ const initMapbox = () => {
     });
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 13, duration: 0 });
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
   }
 };
 
