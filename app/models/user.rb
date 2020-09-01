b05497ccaacbbe31b07bbfb40266ebe306c51cfd
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2020-09-01 12:14:06 +0200

    save user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,7 @@ class User < ApplicationRecord
           .map { |resort| [resort, preference.score(resort.preference)] } # [[#<Resort....>, 67], [......]
           .sort_by { |pair| - pair[1] } # Sorting DESC
           .first(3)
+
+    # [ { resort: resort, score: 54 }, {....}, ......]      suggestion
   end
 end
