5a17c849e8b87bc290694d7401139a068dd27380
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2020-09-02 13:21:13 +0200

    console.log added to check if geoloc is working

--- app/javascript/plugins/georges.js
+++ app/javascript/plugins/georges.js
@@ -227,12 +227,15 @@ const initGeorges = () => {
   const geocationTrue = document.getElementById('geocation-true');
   geocationTrue.addEventListener('click', function check() {
     document.getElementById("preference_geocation").checked = true;
-
+    console.log('is trying to geoloc...')
     // ask browser geoloc
+
+
     navigator.geolocation.getCurrentPosition(function(position) {
       document.getElementById('preference_longitude').value = position.coords.longitude;
       document.getElementById('preference_latitude').value = position.coords.latitude;
 
+      console.log(`got coordinates ${position.coords.longitude} || ${position.coords.latitude}`)
 
     });
 
